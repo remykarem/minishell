57096540e7e05812ad592fc2d3eaf8c12e6d6a7a
raibosome <raimi.bkarim@gmail.com>
2020-05-21 22:52:55 +0800

    Refactor

--- preview.c
+++ preview.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <string.h>
 #include <dirent.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
 
-#define NUM_FILES 10
 #define NBYTES 128
-#define FILENAME_BUFFER 256
 #define MAX_BUFFER 1024
+#define FILENAME_BUFFER 256
+#define NUM_PREVIEW_FILES 10
+#define RED printf("\033[0;31m");
+#define CYAN printf("\033[0;36m");
+#define GREEN printf("\033[0;32m");
+#define YELLOW printf("\033[0;33m");
+#define DEFAULT_COLOUR printf("\033[0m");
+#define DASHES printf("------------\n");
 
-void open_directory(char *filename_, int num_files)
+void open_directory(char *path_to_dir, int num_preview_files)
 {
 	DIR *dir;
 	int nbytes;
-	int counter = 0;
 	int view_fd;
+	int counter = 0;
 	char buffer[MAX_BUFFER];
 	struct stat stat_buffer;
 	struct dirent *dir_entry;
 	int reading_buffer = NBYTES;
 	char *filename = (char *)malloc(FILENAME_BUFFER * sizeof(char));
-	char *full_file_path = (char *)malloc(1 + strlen(filename_) + strlen(filename));
+	char *full_file_path = (char *)malloc(1 + strlen(path_to_dir) + strlen(filename));
 
-	dir = opendir(filename_);
+	dir = opendir(path_to_dir);
 	if (dir == NULL)
 	{
 		perror("Error");
@@ -37,16 +43,14 @@ void open_directory(char *filename_, int num_files)
 	readdir(dir);
 	readdir(dir);
 
-	// Prints `num_files` files in the directory
-	printf("\033[0;36m");
-	printf("------------\n");
-	printf("\033[0;33m");
+	// Prints `num_preview_files` files in the directory
+	CYAN DASHES
+	YELLOW
 	while ((dir_entry = readdir(dir)) != NULL &&
-		   counter++ < num_files)
+		   counter++ < num_preview_files)
 		printf("─ %s\n", dir_entry->d_name);
-	printf("\033[0m");
-	printf("\033[0;36m");
-	printf("------------\n\n");
+	CYAN DASHES
+	printf("\n");
 
 	// Enter interpreter
 	printf("Enter filename to view its contents.\n");
@@ -57,31 +61,34 @@ void open_directory(char *filename_, int num_files)
 		// Get filename from the given list
 		filename = readline("\033[1;36m>>> ");
 		if (filename == NULL)
+		{
+			perror("Error reading input");
 			exit(1);
+		}
 
 		if (strcmp(filename, ".") == 0)
 		{
-			printf("\033[0;31mThat is here!\033[0m\n");
+			RED
+			printf("That is here!\n");
+			DEFAULT_COLOUR
 			continue;
 		}
 		else if (strcmp(filename, "") == 0)
 		{
-			// Prints `num_files` files in the directory
+			// Prints `num_preview_files` files in the directory
 			counter = 0;
-			printf("\033[0;36m");
-			printf("------------\n");
-			printf("\033[0;33m");
+			CYAN DASHES
+			YELLOW
 			while ((dir_entry = readdir(dir)) != NULL &&
-				   counter++ < num_files)
+				   counter++ < num_preview_files)
 				printf("- %s\n", dir_entry->d_name);
-			printf("\033[0m");
-			printf("\033[0;36m");
-			printf("------------\n\n");
+			CYAN DASHES
+			printf("\n");
 			continue;
 		}
 
 		// Get full path to filename
-		strcpy(full_file_path, filename_);
+		strcpy(full_file_path, path_to_dir);
 		strcat(full_file_path, filename);
 		printf("Opening: %s\n", full_file_path);
 
@@ -89,14 +96,18 @@ void open_directory(char *filename_, int num_files)
 		view_fd = open(full_file_path, O_RDONLY);
 		if (view_fd < 0)
 		{
-			printf("\033[0;31mError opening file.\033[0m\n");
+			RED
+			printf("Error opening file.\n");
+			DEFAULT_COLOUR
 			continue;
 		}
 
 		fstat(view_fd, &stat_buffer);
 		if (S_ISDIR(stat_buffer.st_mode) == 1)
 		{
-			printf("\033[0;31mThat's a directory.\033[0m\n");
+			RED
+			printf("That's a directory.\n");
+			DEFAULT_COLOUR
 			close(view_fd);
 			continue;
 		}
@@ -105,17 +116,19 @@ void open_directory(char *filename_, int num_files)
 		if (stat_buffer.st_size < NBYTES)
 			reading_buffer = stat_buffer.st_size;
 
-		// Read and print file
+		// Read and print file contents
 		nbytes = read(view_fd, buffer, reading_buffer);
 		if (nbytes == -1)
 		{
-			printf("\033[0;31m");
-			printf("Error reading file %s.\033[0m\n", filename_);
+			RED
+			printf("Error reading file %s.\n", full_file_path);
+			DEFAULT_COLOUR
 			close(view_fd);
 			continue;
 		}
-		printf("\033[0;32m%.*s\n", reading_buffer, buffer);
-		printf("\033[0m");
+		GREEN
+		printf("%.*s\n", reading_buffer, buffer);
+		DEFAULT_COLOUR
 
 		close(view_fd);
 	}
@@ -127,21 +140,21 @@ void open_directory(char *filename_, int num_files)
 
 int main(int argc, char *argv[])
 {
-	int num_files = NUM_FILES;
+	int num_preview_files = NUM_PREVIEW_FILES;
 
 	if (argc == 3)
 	{
-		sscanf(argv[2], "%d", &num_files);
+		sscanf(argv[2], "%d", &num_preview_files);
 	}
 	else if (argc != 2)
 	{
-		printf("Usage: list path_to_dir [num_files]\n\n");
+		printf("Usage: list path_to_dir [num_preview_files]\n\n");
 		printf("List a specified number of files from a ");
 		printf("specified directory.");
 		exit(EXIT_FAILURE);
 	}
 
-	open_directory(argv[1], num_files);
+	open_directory(argv[1], num_preview_files);
 
 	return 0;
 }
